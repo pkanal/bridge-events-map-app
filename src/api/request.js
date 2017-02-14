3bd881bab884581c810025448a45d6d132918a1b
Purvi Kanal <purvikanal92@gmail.com>
2017-02-14 16:12:30 -0500

    add wrapper functions for eventful api

---
+++ src/api/request.js
@@ -0,0 +1,23 @@
+import 'whatwg-fetch';
+
+import { EVENTFUL_API_BASE_URL, EVENTFUL_API_KEY } from '../config/api';
+import { buildQueryString } from './helpers';
+
+function request(endpoint, method = 'GET', extraHeaders) {
+  return fetch(`${EVENTFUL_API_BASE_URL}/${endpoint}`, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      ...extraHeaders,
+    },
+  }).then(response => response.json());
+}
+
+export function get(endpoint, params) {
+  const queryString = buildQueryString({
+    app_key: EVENTFUL_API_KEY,
+    ...params,
+  });
+  const url = `${endpoint}?${queryString}`;
+  return request(url);
+}
---
+++ src/config/api.js
@@ -0,0 +1,2 @@
+export const EVENTFUL_API_BASE_URL = 'http://api.eventful.com/json';
+export const EVENTFUL_API_KEY = '3RXRMbCtSm89nDRV';
